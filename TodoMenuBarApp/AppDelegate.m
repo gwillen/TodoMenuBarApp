71246fcf368bece8dfa32d00bdcb86b0da63c727
Glenn Willen <gwillen@nerdnet.org>
2014-09-13 02:39:13 -0700

    Initial Commit

---
+++ TodoMenuBarApp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TodoMenuBarApp
+//
+//  Created by Glenn Willen on 9/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
